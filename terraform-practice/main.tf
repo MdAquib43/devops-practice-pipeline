773e610d6454bd5ddc7cb85472d54087421ead7a
MdAquib43 <160226236+MdAquib43@users.noreply.github.com>
2026-08-22 17:04:12 +0000

    Add Terraform CI workflow and clean Terraform files

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -1,8 +1,35 @@
 terraform {
   required_version = ">= 1.0"
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "aquib-terraform-state-237162087889"
+    key          = "terraform-practice/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
-resource "local_file" "hello" {
-  filename = "hello.txt"
-  content  = var.file_content
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "file" {
+  source = "./modules/file"
+
+  file_name    = "module-file.txt"
+  file_content = "Hello from my Terraform module!"
+}
+
+resource "aws_s3_bucket" "import_demo" {
+  bucket = "aquib-terraform-import-237162087889"
 }
